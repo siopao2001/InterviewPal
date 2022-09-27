4661213fe07d773ee49b70a05c9605bb888c2f4e
siopao2001 <sio_pao2001@yahoo.ca>
2022-09-27 19:25:43 -0400

    add framwork for screen navigation, install proper dependencies, create component folder

--- App.js
+++ App.js
@@ -1,22 +1,108 @@
-//Edit this file and review changes in expo via simulator or device
+import * as React from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to Interviewer Pal</Text>
+      <Button
+        title="Quickstart"
+        onPress={() => {
+          /* 1. Navigate to the categories pages */
+          navigation.navigate('Categories');
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function CategoriesScreen({ navigation }) {
+  
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Choose A Category</Text>
+      <Button
+        title="General/Interpersonal"
+        onPress={() =>
+          navigation.navigate('Question', {catId: 1})                    
+        }
+      />
+
+      <Button
+        title="Customer Service Scenarios"
+        onPress={() => {
+          /* 1. Navigate to the categories pages */
+          navigation.navigate('Question', {catId: 2});
+        }}
+      />
+
+      <Button
+        title="IT Technical"
+        onPress={() => {
+          /* 1. Navigate to the categories pages */
+          navigation.navigate('Question', {catId: 3}) 
+          
+        }}
+      />
+
+      <Button
+        title="Managament Scenarios"
+        onPress={() => {
+          /* 1. Navigate to the categories pages */
+          navigation.navigate('Question', {catId: 4});
+        }}
+      />
+    </View>
+  );
+}
+
+
+function QuestionScreen({ route, navigation }) {
+  const { catId, questionId } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>This is the category number for this question: {catId}</Text>
+      <Text>This is the question number: {questionId}</Text>
+      {questionId > 3 
+        ?  <Button title="Summary" onPress={() => {navigation.navigate('Summary')}}/>
+        : <Button
+        title="Next Question"
+        onPress={() => navigation.push('Question', {catId: catId, questionId: questionId + 1})}
+      />
+      }
+    </View>
+  );
+}
+
+function SummaryScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>This is the summary page</Text>
+      <Button
+        title="Start again"
+        onPress={() => {
+          
+          navigation.navigate('Home');
+        }}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Categories" component={CategoriesScreen} />
+        <Stack.Screen name="Question" component={QuestionScreen} initialParams={{ questionId: 1 }}/>
+        <Stack.Screen name="Summary" component={SummaryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
