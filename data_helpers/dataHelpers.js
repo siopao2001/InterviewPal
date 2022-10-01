5cb028deb3a6e440f88caa8a35ca3ce2f4a3e1d5
siopao2001 <sio_pao2001@yahoo.ca>
2022-10-01 08:49:03 -0400

    create data helpers

---
+++ data_helpers/dataHelpers.js
@@ -0,0 +1,44 @@
+//Gets an array of questions based on category ID
+export const getQuestionsByCatId  = (id, array) => {
+  let newArray = [];
+  for (q of array) {
+    if (q.category === id) {
+      newArray.push(q)
+   }
+  }
+return newArray
+  };
+
+//Creates an array of 4 randomized questions
+export const randomValuesFromArray = (array) => {
+  let arrayCopy = [...array]
+  let randomQuestions = []
+    
+  for(let i = 0; i < 4; i++) {
+     const randNum = Math.floor(Math.random() * arrayCopy.length);
+     let splicedItem = arrayCopy.splice(randNum, 1)[0]
+     randomQuestions.push(splicedItem)
+ }
+   return randomQuestions;
+};
+
+//inserts answer object into answer array
+export const addAnswer = (ansText, id, ansArray) => {
+    ansArray.push({
+         answerId: ansArray.length + 1,
+         answer: ansText,
+         questionId: id
+       }
+      )
+};
+
+//retrieves the last answer entered associated with a question
+export const lastAnswerByQuestionID = (array, qId) => {
+   let ansArray = []
+    for(ans of array) {
+       if (ans.questionId === qId) {
+          ansArray.push(ans)
+       }
+    }
+    return ansArray[ansArray.length - 1].answer
+}
