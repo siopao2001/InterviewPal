91806ae58b52989b806df1d5849b9b24eef10244
siopao2001 <sio_pao2001@yahoo.ca>
2022-10-01 08:56:13 -0400

    refactored categories screen function

--- screen-components/CategoriesScreen.js
+++ screen-components/CategoriesScreen.js
@@ -2,44 +2,63 @@ import * as React from 'react';
 import { Text, View, Button } from 'react-native';
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
+import { categories } from './data-harcoded/data'
 
-
-export default function CategoriesScreen({ navigation }) {
+// export default function CategoriesScreen({ navigation }) {
   
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Choose A Category</Text>
-      <Button
-        title="General/Interpersonal"
-        onPress={() =>
-          navigation.navigate('Question', {catId: 1})                    
-        }
-      />
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Choose A Category</Text>
+//       <Button
+//         title="General/Interpersonal"
+//         onPress={() =>
+//           navigation.navigate('Question', {catId: 1})                    
+//         }
+//       />
 
-      <Button
-        title="Customer Service Scenarios"
-        onPress={() => {
-          /* 1. Navigate to the categories pages */
-          navigation.navigate('Question', {catId: 2});
-        }}
-      />
-
-      <Button
-        title="IT Technical"
-        onPress={() => {
-          /* 1. Navigate to the categories pages */
-          navigation.navigate('Question', {catId: 3}) 
+//       <Button
+//         title="Customer Service Scenarios"
+//         onPress={() => {
           
-        }}
-      />
+//           navigation.navigate('Question', {catId: 2});
+//         }}
+//       />
 
-      <Button
-        title="Managament Scenarios"
-        onPress={() => {
-          /* 1. Navigate to the categories pages */
-          navigation.navigate('Question', {catId: 4});
-        }}
-      />
-    </View>
-  );
+//       <Button
+//         title="IT Technical"
+//         onPress={() => {
+          
+//           navigation.navigate('Question', {catId: 3}) 
+          
+//         }}
+//       />
+
+//       <Button
+//         title="Managament Scenarios"
+//         onPress={() => {
+          
+//           navigation.navigate('Question', {catId: 4});
+//         }}
+//       />
+//     </View>
+//   );
+// }
+
+export function CategoriesScreen({navigation}) {
+   const categoriesList = categories.map((cat) => 
+            <Button
+                title={cat.catName}
+             onPress={() => {
+                 let QuestionsByCategory = getQuestionsByCatId(cat.id, questions)
+                 let randomQuestionsArray = randomValuesFromArray(QuestionsByCategory);
+                 navigation.navigate('Question', {array: randomQuestionsArray})  
+             }                  
+            }
+             />
+          );
+   return (
+     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {categoriesList}
+     </View>
+   )
 }
