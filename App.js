5981e1145c49bded09546d660a72851ba7831bfa
roop <rplawn@icloud.com>
2022-10-01 21:50:39 -0700

    final merge changes *working*

--- App.js
+++ App.js
@@ -2,42 +2,24 @@ import * as React from 'react';
 import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import Login from './components/login';
 import Signup from './components/signup';
 import Dashboard from './components/dashboard';
 const Stack = createStackNavigator();
+import { registerRootComponent } from 'expo';
 import HomeScreen from './screen-components/HomeScreen';
 import CategoriesScreen from './screen-components/CategoriesScreen';
 import QuestionScreen from './screen-components/QuestionScreen';
 import SummaryScreen from './screen-components/SummaryScreen';
 
-
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Categories" component={CategoriesScreen} />
-        <Stack.Screen name="Question" component={QuestionScreen} initialParams={{ questionIndex: 0 }}/>
-        <Stack.Screen name="Summary" component={SummaryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 function MyStack() {
   return (
@@ -75,10 +57,19 @@ function MyStack() {
     </Stack.Navigator>
   );
 }
+
 export default function App() {
   return (
     <NavigationContainer>
       <MyStack />
+      {/* <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Categories" component={CategoriesScreen} />
+        <Stack.Screen name="Question" component={QuestionScreen} initialParams={{ questionIndex: 0 }}/>
+        <Stack.Screen name="Summary" component={SummaryScreen} />
+      </Stack.Navigator> */}
     </NavigationContainer>
   );
 }
+
+registerRootComponent(App);
