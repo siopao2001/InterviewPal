cc103731c035518453c78604af6fe98fab8b0af1
roop <rplawn@icloud.com>
2022-10-06 15:47:03 -0700

    useState and useEffect to pull API request for categories

--- screen-components/CategoriesScreen.js
+++ screen-components/CategoriesScreen.js
@@ -1,55 +1,34 @@
 import * as React from 'react';
 import { Text, View, Button } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-import { categories, questions } from '../data-hardcoded/data';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { getQuestionsByCatId, randomValuesFromArray } from '../data_helpers/dataHelpers';
 
-// export default function CategoriesScreen({ navigation }) {
-  
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Choose A Category</Text>
-//       <Button
-//         title="General/Interpersonal"
-//         onPress={() =>
-//           navigation.navigate('Question', {catId: 1})                    
-//         }
-//       />
-
-//       <Button
-//         title="Customer Service Scenarios"
-//         onPress={() => {
-          
-//           navigation.navigate('Question', {catId: 2});
-//         }}
-//       />
-
-//       <Button
-//         title="IT Technical"
-//         onPress={() => {
-          
-//           navigation.navigate('Question', {catId: 3}) 
-          
-//         }}
-//       />
-
-//       <Button
-//         title="Managament Scenarios"
-//         onPress={() => {
-          
-//           navigation.navigate('Question', {catId: 4});
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
 export default function CategoriesScreen({navigation}) {
+   const [categories, setCategories] = useState([]);
+   const [questions, setQuestions] = useState([]);
+   useEffect(() => {
+      axios.get("http://localhost:8080/categories")
+      .then((response) => {
+         setCategories(response.data)
+      }).catch((error) => {
+         console.log("Error", error)
+      })
+   },[])
+
+   useEffect(() => {
+      axios.get("http://localhost:8080/questions")
+      .then((response) => {
+         setQuestions(response.data)
+      }).catch((error) => {
+         console.log("Error", error)
+      })
+   },[])
+
    const categoriesList = categories.map((cat) => 
             <Button
                 key={cat.id}
-                title={cat.catName}
+                title={cat.name}
              onPress={() => {
                  let QuestionsByCategory = getQuestionsByCatId(cat.id, questions)
                  let randomQuestionsArray = randomValuesFromArray(QuestionsByCategory);
