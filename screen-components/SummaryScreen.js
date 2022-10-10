d9ac313eef1a36b39e99e546286c656bf8d8fb75
roop <rplawn@icloud.com>
2022-10-09 19:09:51 -0700

    cleaned up test code

--- screen-components/SummaryScreen.js
+++ screen-components/SummaryScreen.js
@@ -1,32 +1,14 @@
 import * as React from 'react';
 import {useState, useEffect} from 'react';
 import { Text, View, Button } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
 import { lastAnswerByQuestionID } from '../data_helpers/dataHelpers';
 import axios from 'axios';
 
-// export default function SummaryScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>This is the summary page</Text>
-//       <Button
-//         title="Start again"
-//         onPress={() => {
-          
-//           navigation.navigate('Home');
-//         }}
-//       />
-//     </View>
-//   );
-// }
 
 export default function SummaryScreen({ route, navigation }) {
   const { array } = route.params;
   const [answers, setAnswers] = useState([]);
   const array1 = [...answers];
-  //const test = lastAnswerByQuestionID(array1, 1)
-  //console.log(test)
 
   useEffect(() => {
       axios.get("http://localhost:8080/answers")
