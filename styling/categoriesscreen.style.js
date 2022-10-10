1c038e9bb6561f71f895f01451d47a82acd7bb7a
roop <rplawn@icloud.com>
2022-10-10 01:28:21 -0700

    separated categories screen styling

---
+++ styling/categoriesscreen.style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+ 
+  bottom: {
+    color: '#fff',
+    borderWidth: 5,
+    borderColor: 'grey',
+    padding: 2,
+    backgroundColor: '#CF9FFF',
+    margin: 5,
+  }
+});
